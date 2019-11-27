ad18d6bdc980f050c9be403a708c2f8abc3e0455
systemax-andre <systemax-andre@grupotechnos.com.br>
2019-11-27 14:59:58 -0400

    Arquitetura

---
+++ service/Database/IDatabaseService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace service.Database
+{
+    public interface IDatabaseService
+    {
+        string GetDefaultConnectionString();
+
+        string GetConnectionString(dynamic connectionId);
+
+        IEnumerable<dynamic> GetConnectionStrings();
+    }
+}
